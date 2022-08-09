f908399071168d9a81858aad18ca2c46e85ca8a4
CetO-hub <skynet@SkyNet.localdomain>
2022-08-09 18:57:25 +0200

    Add account and saved shows feature

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,7 +1,23 @@
 import React from "react";
+import SavedShows from "../components/SavedShows";
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className="  w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/c8c8a0ad-86d6-45f1-b21d-821afa4e5027/1657f8a1-7017-4b78-abfb-0db5308527eb/DE-de-20220801-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt=""
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
 };
 
 export default Account;
